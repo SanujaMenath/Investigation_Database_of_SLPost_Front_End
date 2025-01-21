605ad6950d4564a23c795778a4550c1c5b71bffb
sanujamenath <sanujamenath@gmail.com>
2025-01-21 20:36:25 +0530

    Create Suspect's details form

--- src/pages/CreateInvestigation.tsx
+++ src/pages/CreateInvestigation.tsx
@@ -4,13 +4,16 @@ import FormalInquiry from "../components/forms/FormalInquiry";
 import SecondaryFields from "../components/forms/SecondaryFields";
 import Header from "../components/ui/Header";
 import Chargesheet from "../components/forms/Chargesheet";
+import CreateSuspector from "../components/forms/CreateSuspector";
 
 function CreateInvestigation() {
   return (
     <div>
       <Header />
       <div className="flex flex-col items-center min-h-screen bg-indigo-50">
-        <h1 className="text-4xl text-indigo-900 font-medium mt-4">Create Investigation</h1>
+        <h1 className="text-4xl text-indigo-900 font-medium mt-4">
+          Create Investigation
+        </h1>
 
         <form className="space-y-4 p-4 mx-auto w-full max-w-3xl border rounded mb-4 mt-6">
           <MandatoryFields />
@@ -18,6 +21,7 @@ function CreateInvestigation() {
           <InterimReport />
           <Chargesheet />
           <FormalInquiry />
+          <CreateSuspector />
         </form>
       </div>
     </div>
